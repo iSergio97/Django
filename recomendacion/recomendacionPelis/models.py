19de4db3f0e403294742da3dfa680f7fbbbd3c93
Sergio Garrido <sergiotb15@gmail.com>
2019-11-25 20:46:12 +0100

    Sorry not sorry

--- recomendacion/recomendacionPelis/models.py
+++ recomendacion/recomendacionPelis/models.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from .models import *
-from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse
+from django.db import models
 
 # Create your models here.
 
-def inicio(request):
-    return render_to_response('index.html', {})
+class Pelicula(models.Model):
+    idPelicula = models.IntegerField(primary_key= True)
+    titulo = models.CharField(max_length=100)
+    fechaEstreno = models.DateField()
+    IMDbURL = models.URLField()
+    categorias =
+
+
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key= True)
+    nombre = models.CharField(max_length=50)
