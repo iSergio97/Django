7205387f6804c7a1baf78a973cc177e0107dcc40
Carlos <carlosrb96@gmail.com>
2019-11-25 21:00:31 +0100

    Usuario y Ocupacion

--- recomendacion/recomendacionPelis/models.py
+++ recomendacion/recomendacionPelis/models.py
@@ -13,3 +13,16 @@ class Pelicula(models.Model):
 class Categoria(models.Model):
     idCategoria = models.IntegerField(primary_key= True)
     nombre = models.CharField(max_length=50)
+
+class Ocupacion(models.Model):
+    nombre = models.CharField(max_length= 50)
+
+class Usuario(models.Model):
+    idUsuario = models.IntegerField(primary_key= True)
+    edad = models.IntegerField()
+    sexo = models.CharField(max_length=1)
+    ocupacion = models.ForeignKey(Ocupacion, on_delete=models.CASCADE)
+    codigoPostal = models.IntegerField()
+
+
+
