763eb95581565f5b8d7f72beb2ef08be68f886c3
Sergio Garrido <sergiotb15@gmail.com>
2020-01-13 20:03:17 +0100

    Models

--- practicaResSysc/resSysC/models.py
+++ practicaResSysc/resSysC/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Anime(models.Model):
+    animeId = models.BigIntegerField(primary_key=True)
+    titulo = models.TextField()
+    generos = models.TextField()
+    formatoEmision = models.TextField()
+    numEpisodios = models.IntegerField()
+
+
+class Puntuacion(models.Model):
+    idUsuario = models.BigIntegerField()
+    animeId = models.ForeignKey("Anime", on_delete=models.CASCADE)
+    puntuacion = models.IntegerField()
