26ee0789997bc9936e72cdc1d7b826bd1c153e9e
Sergio Garrido <sergiotb15@gmail.com>
2020-01-16 14:45:20 +0100

    Añadido modelos y una pequeña prueba en la bbdd para probar que los modelos funcionan correctamente

--- sistemasMedicos/main/admin.py
+++ sistemasMedicos/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Tema)
+admin.site.register(Foro)
+admin.site.register(TemaFavoritoUsuario)
+admin.site.register(Respuesta)
--- sistemasMedicos/main/models.py
+++ sistemasMedicos/main/models.py
@@ -2,37 +2,44 @@ from django.db import models
 
 # Create your models here.
 '''
-
 https://medicinaysaludpublica.com/categoria/articulos/
 
 https://medicinaysaludpublica.com/categoria/noticias/
-
-Modelos:
-Se añade de un CSV
-Foro:
-    -ID
-    -Nombre
-
-WebScrapping
-Tema:
-    -Título
-    -Autor puede ser nulo si la noticia es de web scrapping
-    -NumRespuestas
-    -Fecha última respuesta
-    -Foro al que pertenece
-
-WebScrapping    
-Respuesta:
-    -Autor
-    -Fecha y hora
-    -Texto
-    -Tema al que responde
-
-
-Usuario:
-    -Nombre
-    -Apellido
-    -Email
-    -Nombre de usuario
-    -Contraseña
 '''
+
+class Usuario(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.TextField()
+    apellido = models.TextField()
+    email = models.EmailField()
+    username = models.TextField(unique=True)
+    contraseña = models.TextField()
+    temasFavoritos = models.ManyToManyField('Tema', related_name='TemaFavoritoUsuario', blank=True)
+
+
+class Foro(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.TextField()
+
+
+class Tema(models.Model):
+    id = models.IntegerField(primary_key=True)
+    titulo = models.TextField()
+    usuario = models.ForeignKey('Usuario', null=True, on_delete=models.CASCADE)
+    numRespuestas = models.IntegerField()
+    ultimaRespuesta = models.DateField(null=True, blank=True)
+    foroOrigen = models.ForeignKey('Foro', null=False, on_delete=models.CASCADE)
+
+
+class Respuesta(models.Model):
+    id = models.IntegerField(primary_key=True)
+    usuario = models.ForeignKey('Usuario', null=True, on_delete=models.CASCADE)
+    fechaHora = models.DateTimeField()
+    texto = models.TextField()
+    temaOrigen = models.ForeignKey('Tema', null=False, on_delete=models.CASCADE)
+
+
+class TemaFavoritoUsuario(models.Model):
+    id = models.IntegerField(primary_key=True)
+    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    tema = models.ForeignKey('Tema', on_delete=models.CASCADE)
