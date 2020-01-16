1e64e71f10bac26e953a4d029507a68d67940ef6
Sergio Garrido <sergiotb15@gmail.com>
2020-01-16 14:50:43 +0100

    Eliminado los on_delete cascade para que no se borren todos los objetos relacionados

--- sistemasMedicos/main/models.py
+++ sistemasMedicos/main/models.py
@@ -25,21 +25,21 @@ class Foro(models.Model):
 class Tema(models.Model):
     id = models.IntegerField(primary_key=True)
     titulo = models.TextField()
-    usuario = models.ForeignKey('Usuario', null=True, on_delete=models.CASCADE)
+    usuario = models.ForeignKey('Usuario', null=True)
     numRespuestas = models.IntegerField()
     ultimaRespuesta = models.DateField(null=True, blank=True)
-    foroOrigen = models.ForeignKey('Foro', null=False, on_delete=models.CASCADE)
+    foroOrigen = models.ForeignKey('Foro', null=False)
 
 
 class Respuesta(models.Model):
     id = models.IntegerField(primary_key=True)
-    usuario = models.ForeignKey('Usuario', null=True, on_delete=models.CASCADE)
+    usuario = models.ForeignKey('Usuario', null=True)
     fechaHora = models.DateTimeField()
     texto = models.TextField()
-    temaOrigen = models.ForeignKey('Tema', null=False, on_delete=models.CASCADE)
+    temaOrigen = models.ForeignKey('Tema', null=False)
 
 
 class TemaFavoritoUsuario(models.Model):
     id = models.IntegerField(primary_key=True)
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
-    tema = models.ForeignKey('Tema', on_delete=models.CASCADE)
+    usuario = models.ForeignKey('Usuario')
+    tema = models.ForeignKey('Tema')
